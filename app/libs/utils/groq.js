0c707292a1dbbf6551e27850cae7eeae148fcf1f
vansh potpose <vanshpotpose@gmail.com>
2025-01-10 18:55:14 +0530

    updated

--- app/libs/utils/groq.js
+++ app/libs/utils/groq.js
@@ -1,7 +1,7 @@
 import Groq from "groq-sdk";
 
 const groq = new Groq({
-  apiKey: process.env.NEXT_PUBLIC_GROQ_API_KEY,
+  apiKey: process.env.GROQ_API_KEY,
 });
 
 export const reqGroqAI = async (content) => {
