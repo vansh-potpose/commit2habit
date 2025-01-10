1d561a89c0b50117dae301b7d36a072238e33308
vansh potpose <vanshpotpose@gmail.com>
2025-01-10 18:15:38 +0530

    updated deploy according nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, ''); // Extract repo name
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
-  output: 'export', // Static export
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
+  output:"export"
 };
-
 export default nextConfig;
